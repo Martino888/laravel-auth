512f574ffc44a7e2885b41927363e2cdc0c9ad09
Martino888 <maxmartino33@gmail.com>
2022-05-18 17:58:15 +0200

    creazione file in views

--- app/Post.php
+++ app/Post.php
@@ -3,8 +3,25 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
 
 class Post extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'description',
+        'slug',
+    ];
+
+    static public function genSlug($data) {
+        $elementSlug = Str::of($data)->slug('-');
+        $slug = $elementSlug;
+        $i = 1;
+        while(self::where('slug', $slug)->first()) {
+            $slug = "$elementSlug-$i";
+            $i++;
+        }
+        return $slug;
+    }
 }
